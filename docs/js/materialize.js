c4a5cb92a9dffb2fe52e1f17a251c11c104f0d91
alu0100658705 <43812499+alu0100658705@users.noreply.github.com>
2019-03-25 14:54:12 +0000

    Add files via upload

--- docs/js/materialize.js
+++ docs/js/materialize.js
@@ -12377,3 +12377,5 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems, options);
   });
+
+
